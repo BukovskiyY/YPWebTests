006b69e75a6427d6113f6149de1239eea4b03d8f
kolob <kolob3077@gmail.com>
2026-01-13 17:47:14 +0300

    Реализовал паттерн PageObject

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -1,8 +1,16 @@
+from faker import Faker
 import pytest
 from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
     driver = webdriver.Chrome()
+    driver.maximize_window()
     yield driver
     driver.quit()
+
+@pytest.fixture
+def mail_login():
+    faker = Faker()
+    login = faker.email()
+    return login
