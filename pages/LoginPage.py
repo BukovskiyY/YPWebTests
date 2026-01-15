3580a14ecab675a4b98800e00d4edb8133bbc3d6
kolob <kolob3077@gmail.com>
2026-01-15 12:14:32 +0300

    Добавил allure

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,3 +1,5 @@
+import allure
+
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
@@ -7,13 +9,12 @@ class LoginPageLocators:
     LOGIN_EMAIL = (By.ID, 'field_email')
     LOGIN_PASSWORD = (By.ID, 'field_password')
     LOGIN_BUTTON = (By.XPATH, '//*[@label="Войти"]')
-    # LOGIN_BY_QR_CODE = (By.XPATH, '//*[@label="Войти по QR-коду"]')
-    # RESTORE_LINK = (By.XPATH, '//*[@data-l="t,restore"]')
-    # REGISTRATION_BUTTON = (By.XPATH, '//div[@class="external-oauth-login-footer"]/a[@data-l="t,register"]')
-    # VK_BUTTON = (By.XPATH, '//*[@data-l="t,vkc"]')
-    # MAIL_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
-    # YANDEX_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
-    # OTHER_BUTTON = (By.XPATH, '//*[@data-l="t,other"]')
+    LOGIN_BY_QR_CODE = (By.XPATH, '//*[@label="Войти по QR-коду"]')
+    RESTORE_LINK = (By.XPATH, '//*[contains(@href, "anonymRecoveryStart")]')
+    REGISTRATION_BUTTON = (By.XPATH, '//button[.//span[text()="Зарегистрироваться"]]')
+    VK_BUTTON = (By.XPATH, '//*[@data-l="t,vkc"]')
+    MAIL_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
+    YANDEX_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
     ERROR_TEXT = (By.CSS_SELECTOR, "span[class*='LoginForm-module__error']")
 
 
@@ -28,19 +29,23 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.LOGIN_EMAIL)
         self.find_element(LoginPageLocators.LOGIN_PASSWORD)
         self.find_element(LoginPageLocators.LOGIN_BUTTON)
-        # self.find_element(LoginPageLocators.LOGIN_BY_QR_CODE)
-        # self.find_element(LoginPageLocators.RESTORE_LINK)
-        # self.find_element(LoginPageLocators.REGISTRATION_BUTTON)
-        # self.find_element(LoginPageLocators.VK_BUTTON)
-        # self.find_element(LoginPageLocators.MAIL_BUTTON)
-        # self.find_element(LoginPageLocators.YANDEX_BUTTON)
-        # self.find_element(LoginPageLocators.OTHER_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_CODE)
+        self.find_element(LoginPageLocators.REGISTRATION_BUTTON)
+        self.find_element(LoginPageLocators.VK_BUTTON)
+        self.find_element(LoginPageLocators.MAIL_BUTTON)
+        self.find_element(LoginPageLocators.YANDEX_BUTTON)
 
+    @allure.step('Нажимаем на кнопку "Войти"')
     def click_login(self):
+        self.attach_screenshot()
         self.find_element(LoginPageLocators.LOGIN_BUTTON).click()
 
+    @allure.step('Получаем текст ошибки')
     def get_error_text(self):
+        self.attach_screenshot()
         return  self.find_element(LoginPageLocators.ERROR_TEXT).text
 
+    @allure.step('Заполняем поле login')
     def send_login(self, mail_login):
         self.find_element(LoginPageLocators.LOGIN_EMAIL).send_keys(mail_login)
+        self.attach_screenshot()
